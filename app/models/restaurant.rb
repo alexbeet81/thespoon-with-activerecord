f0c046fe6f6fd267ea475c069fc735f3abe12384
Alex Pritchard <alexbeet1234@gmail.com>
2021-05-25 11:28:12 +0800

    index page and seed added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :rating, inclusion: { to: 1..10 }
+  # validates :name, presence: true
+  # validates :address, presence: true
+  # validates :rating, inclusion: { to: 1..10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD
+  # verb 'path' to: 'controller#action'
+  # Read all restaurants
+  # get '/restaurants', to: 'restaurants#index'
+  # # Read one restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # # Create a restaurant
+  # get '/restaurants/new', to: 'restaurants#new' # display form
+  # post '/restaurants', to: 'restaurants#create'
+  # # Update a restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # # Delete a restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
